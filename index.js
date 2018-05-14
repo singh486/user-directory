965a284e751ad467ea555a31a49009c7596e7f25
Jasmine <singh486@purdue.edu>
2018-05-14 16:28:50 -0400

    Change look and paragraph content

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const button = document.querySelector('#megaButton')
+const button = document.querySelector('button.greeting')
+
 
 function changeText(){
-    alert('yeah')
+    const paragraph = document.querySelector('p.greeting')
+    paragraph.textContent = 'You done clicked!'
 }
 
 button.addEventListener('click', changeText)
+
